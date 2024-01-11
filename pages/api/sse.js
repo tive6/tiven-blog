8907f928c0a009d0d3a56d9056814d8939f24d43
zxf <zxf@yc.com>
2024-01-11 17:24:12 +0800

    feat: 配置 api/sse 接口 最大超时时间 maxDuration 14

--- pages/api/sse.js
+++ pages/api/sse.js
@@ -1,5 +1,3 @@
-import { nanoid } from 'nanoid'
-
 export default function GET(req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/event-stream; charset=utf-8',
@@ -14,23 +12,24 @@ export default function GET(req, res) {
       timestamp: new Date().toISOString(),
     })}\n\nid: 1\n\n`
   )
-  res.write(`event: update\ndata: bye-bye\n\nid: ${nanoid(6)}\n\n`)
-  res.write('event: hi\ndata: hi\n\nid: 100\n\n')
-  let n = 0
-  // res.end('event: hi\ndata: hi\n\nid: 300\n\n')
-  let t = setInterval(() => {
-    n++
-    if (n > 5) {
-      clearInterval(t)
-      res.end('event: end\ndata: 结束通信。\n\nid: 9999\n\n')
-    } else {
-      let id = nanoid(6)
-      let data = JSON.stringify({
-        id,
-        message: `hi，这是第 ${n} 条消息。`,
-        timestamp: new Date().toISOString(),
-      })
-      res.write(`data: ${data}\n\nid: ${id}\n\n`)
-    }
-  }, 2000)
+  res.end()
+  // res.write(`event: update\ndata: bye-bye\n\nid: ${nanoid(6)}\n\n`)
+  // res.write('event: hi\ndata: hi\n\nid: 100\n\n')
+  // let n = 0
+  // // res.end('event: hi\ndata: hi\n\nid: 300\n\n')
+  // let t = setInterval(() => {
+  //   n++
+  //   if (n > 5) {
+  //     clearInterval(t)
+  //     res.end('event: end\ndata: 结束通信。\n\nid: 9999\n\n')
+  //   } else {
+  //     let id = nanoid(6)
+  //     let data = JSON.stringify({
+  //       id,
+  //       message: `hi，这是第 ${n} 条消息。`,
+  //       timestamp: new Date().toISOString(),
+  //     })
+  //     res.write(`data: ${data}\n\nid: ${id}\n\n`)
+  //   }
+  // }, 2000)
 }
