cb441729203a853e5fa57cd42d309108023d8ef3
zxf <zxf@yc.com>
2024-01-11 17:48:07 +0800

    test: setInterval api 测试 5

--- pages/api/sse.js
+++ pages/api/sse.js
@@ -1,3 +1,5 @@
+import { nanoid } from 'nanoid'
+
 export default function GET(req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/event-stream; charset=utf-8',
@@ -12,38 +14,22 @@ export default function GET(req, res) {
       timestamp: new Date().toISOString(),
     })}\n\nid: 1\n\n`
   )
-  console.log(setInterval)
-  console.log(setTimeout)
-
-  console.log(0)
-  try {
-    setTimeout(() => {
-      console.log(789)
-      console.log(Date.now())
-      res.end('event: end\n\ndata: end\n\nid: 9999\n\n')
-      console.log(123)
-    }, 2000)
-  } catch (err) {
-    console.log(err)
-  }
-  console.log(2, Date.now())
-  // res.write(`event: update\ndata: bye-bye\n\nid: ${nanoid(6)}\n\n`)
-  // res.write('event: hi\ndata: hi\n\nid: 100\n\n')
-  // let n = 0
-  // // res.end('event: hi\ndata: hi\n\nid: 300\n\n')
-  // let t = setInterval(() => {
-  //   n++
-  //   if (n > 5) {
-  //     clearInterval(t)
-  //     res.end('event: end\ndata: 结束通信。\n\nid: 9999\n\n')
-  //   } else {
-  //     let id = nanoid(6)
-  //     let data = JSON.stringify({
-  //       id,
-  //       message: `hi，这是第 ${n} 条消息。`,
-  //       timestamp: new Date().toISOString(),
-  //     })
-  //     res.write(`data: ${data}\n\nid: ${id}\n\n`)
-  //   }
-  // }, 2000)
+  res.write(`event: update\ndata: bye-bye\n\nid: ${nanoid(6)}\n\n`)
+  res.write('event: hi\ndata: hi\n\nid: 100\n\n')
+  let n = 0
+  let t = setInterval(() => {
+    n++
+    if (n > 5) {
+      clearInterval(t)
+      res.end('event: end\ndata: 结束通信。\n\nid: 9999\n\n')
+    } else {
+      let id = nanoid(6)
+      let data = JSON.stringify({
+        id,
+        message: `hi，这是第 ${n} 条消息。`,
+        timestamp: new Date().toISOString(),
+      })
+      res.write(`data: ${data}\n\nid: ${id}\n\n`)
+    }
+  }, 1000)
 }
