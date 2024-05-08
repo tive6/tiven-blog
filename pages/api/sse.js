1a98ec50da4b7e63b1c6505ca10b927ab6d911cb
zxf <zxf@yc.com>
2024-05-08 18:32:00 +0800

    feat: sse流式接口配置

--- pages/api/sse.js
+++ pages/api/sse.js
@@ -3,9 +3,11 @@ import { nanoid } from 'nanoid'
 export default function GET(req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/event-stream; charset=utf-8',
-    'Cache-Control': 'no-cache',
+    'Cache-Control': 'no-cache, no-transform',
+    // 'Content-Encoding': 'none',
     Connection: 'keep-alive',
   })
+  // res.flushHeaders(); // 确保头部立即发送
   // res.status(200).json({ text: 'Hello' })
   res.write(
     `data: ${JSON.stringify({
